0627b0a6ec2d9acc108aff3f0d8477a3738cc512
b1674927 <b1674927@protonmail.com>
2023-10-12 17:02:55 +0200

    fix views

--- src/db/migrations/0006_create_last_payment_view.up.sql
+++ src/db/migrations/0006_create_last_payment_view.up.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE VIEW referral_last_payment AS
 SELECT 
     pool_id,
-	trader_addr,
+	LOWER(trader_addr) as trader_addr,
 	BOOL_AND(tx_confirmed) as tx_confirmed, -- false if there are payments that haven't been confirmed yet
 	MAX(block_ts) as last_payment_ts 
-FROM referral_payment GROUP BY trader_addr, pool_id;
+FROM referral_payment GROUP BY LOWER(trader_addr), pool_id;

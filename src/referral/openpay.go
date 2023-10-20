d7c132aa9ad759c7dd7b8994d38cc5c5f5265809
b1674927 <b1674927@protonmail.com>
2023-10-20 16:28:44 +0200

    refill rate

--- src/referral/openpay.go
+++ src/referral/openpay.go
@@ -153,8 +153,8 @@ func (a *App) ProcessAllPayments() error {
 	// Filter blockchain events to confirm payments
 	a.SavePayments()
 	a.PurgeUnconfirmedPayments()
-	// Create a token bucket with a limit of 10 tokens and a refill rate of 5 tokens per second
-	a.PaymentExecutor.NewTokenBucket(10, 5)
+	// Create a token bucket with a limit of 5 tokens and a refill rate of 3 tokens per second
+	a.PaymentExecutor.NewTokenBucket(5, 3)
 	// determine batch timestamp
 	var batchTs string
 

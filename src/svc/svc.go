3d086aedc1d47b228117ccff25a216c131d9c862
b1674927 <b1674927@protonmail.com>
2023-10-23 19:09:22 +0200

    schedule payment at start

--- src/svc/svc.go
+++ src/svc/svc.go
@@ -74,6 +74,10 @@ func Run() {
 	if hasFinished, _ := app.DbGetPaymentExecHasFinished(); !hasFinished || app.IsPaymentDue() {
 		// application crashed before payment was finalized, so restart
 		go app.ProcessAllPayments(false)
+	} else {
+		// schedule payment
+		slog.Info("Scheduling next payment")
+		app.SchedulePayment()
 	}
 	wg.Add(1)
 	go api.StartApiServer(&app, v.GetString(env.API_BIND_ADDR), v.GetString(env.API_PORT), &wg)

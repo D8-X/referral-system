7ff89fe87ed58e12f20c5151eac37aa54f866858
b1674927 <b1674927@protonmail.com>
2023-10-23 19:08:54 +0200

    cosmetics

--- src/referral/payexec.go
+++ src/referral/payexec.go
@@ -227,7 +227,7 @@ func (exc *RemotePayExec) remoteGetSignature(paydata d8x_futures.PaySummary) (st
 		slog.Error("Error reading response body:" + err.Error())
 		return "", err
 	}
-	slog.Info("Remote broker signature obtained.")
+	slog.Info("Remote broker response obtained.")
 	type Response struct {
 		BrokerSignature string `json:"brokerSignature"`
 		Error           string `json:"error"`
